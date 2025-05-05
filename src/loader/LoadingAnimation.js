9f17975f291511e29a54a8eeb300ea1202740d68
akito-akatsuki <mgniichan@gmail.com>
2025-05-05 11:44:31 +0700

    remove load animation

--- src/loader/LoadingAnimation.js
+++ src/loader/LoadingAnimation.js
@@ -3,17 +3,17 @@ import "./style.css"; // Import CSS loader
 
 const LoadingAnimation = () => {
   return (
-    <div className="preloader-2">
-      <span className="line line-1"></span>
-      <span className="line line-2"></span>
-      <span className="line line-3"></span>
-      <span className="line line-4"></span>
-      <span className="line line-5"></span>
-      <span className="line line-6"></span>
-      <span className="line line-7"></span>
-      <span className="line line-8"></span>
-      <span className="line line-9"></span>
-      <div>Loading...</div>
+    <div class="preloader-2">
+      <span class="line line-1"></span>
+      <span class="line line-2"></span>
+      <span class="line line-3"></span>
+      <span class="line line-4"></span>
+      <span class="line line-5"></span>
+      <span class="line line-6"></span>
+      <span class="line line-7"></span>
+      <span class="line line-8"></span>
+      <span class="line line-9"></span>
+      <div>Loading</div>
     </div>
   );
 };
